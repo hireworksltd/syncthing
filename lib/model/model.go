de2337bfb8eadcc8637b48fc3841c4773121719a
Hireworks <jon@hireworks.tv>
2024-10-30 13:53:16 +0000

    Update model.go

--- lib/model/model.go
+++ lib/model/model.go
@@ -2889,7 +2889,7 @@ func (m *model) availability(cfg config.FolderConfiguration, snap *db.Snapshot, 
 	var candidates []Availability
 
 	candidates = append(candidates, m.availabilityInSnapshotRLocked(cfg, snap, file)...)
-	candidates = append(candidates, m.availabilityInTemporaryRLocked(cfg, file, block)...)
+	candidates = append(candidates, m.availabilityFromTemporaryRLocked(cfg, file, block)...)
 
 	return candidates
 }
@@ -2917,7 +2917,7 @@ func (m *model) availabilityInSnapshotRLocked(cfg config.FolderConfiguration, sn
 	return availabilities
 }
 
-func (m *model) availabilityInTemporaryRLocked(cfg config.FolderConfiguration, file protocol.FileInfo, block protocol.BlockInfo) []Availability {
+func (m *model) availabilityFromTemporaryRLocked(cfg config.FolderConfiguration, file protocol.FileInfo, block protocol.BlockInfo) []Availability {
 	var availabilities []Availability
 	for _, device := range cfg.Devices {
 		if m.deviceDownloads[device.DeviceID].Has(cfg.ID, file.Name, file.Version, int(block.Offset/int64(file.BlockSize()))) {
